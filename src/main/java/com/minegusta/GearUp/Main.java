21327f199595b2248cf3cddcc3f75f73b3f51e78
Jan <janie177@hotmail.com>
2014-04-28 17:36:10 +0200

    Initial commit

---
+++ src/main/java/com/minegusta/GearUp/Main.java
@@ -0,0 +1,18 @@
+package com.minegusta.GearUp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+
+	}
+
+	@Override
+	public void onDisable()
+	{
+
+	}
+}
